a2ad75ec89fa7d7f5067beaf1d39511e35083589
hadishamoun1 <shamounhadi2@gmail.com>
2024-10-30 20:44:38 +0200

    Added controller crud

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { Customer } from '../entities/customer.entity';
 
-@Controller('customer')
-export class CustomerController {}
+@Controller('customers')
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Post()
+  create(@Body() data: Partial<Customer>): Promise<Customer> {
+    return this.customerService.create(data);
+  }
+
+  @Get()
+  findAll(): Promise<Customer[]> {
+    return this.customerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<Customer> {
+    return this.customerService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() data: Partial<Customer>): Promise<Customer> {
+    return this.customerService.update(id, data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Promise<void> {
+    return this.customerService.delete(id);
+  }
+}
