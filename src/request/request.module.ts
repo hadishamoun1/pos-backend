c04f1a6d7edffe183696083ee476972a9add5329
hadishamoun1 <shamounhadi2@gmail.com>
2024-10-30 23:10:28 +0200

    Fixed import error

--- src/request/request.module.ts
+++ src/request/request.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Request } from '../entities/request.entity';
-import { RequestItem } from '../entities/request-item.entity';
+import { RequestItem } from '../entities/requestItem.entity';
 import { RequestService } from './request.service';
 import { RequestController } from './request.controller';
 
