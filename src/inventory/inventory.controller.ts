60f4419f4819e84115bee1f4d5450577a9d4bdd9
hadishamoun1 <shamounhadi2@gmail.com>
2024-10-30 19:19:53 +0200

    Created the inventory controller

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { InventoryService } from './inventory.service';
+import { Inventory } from '../entities/inventory.entity';
 
 @Controller('inventory')
-export class InventoryController {}
+export class InventoryController {
+  constructor(private readonly inventoryService: InventoryService) {}
+
+  // Create a new inventory item
+  @Post()
+  create(@Body() data: Partial<Inventory>): Promise<Inventory> {
+    return this.inventoryService.create(data);
+  }
+
+  // Retrieve all inventory items
+  @Get()
+  findAll(): Promise<Inventory[]> {
+    return this.inventoryService.findAll();
+  }
+
+  // Retrieve a single inventory item by ID
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<Inventory> {
+    return this.inventoryService.findOne(id);
+  }
+
+  // Update an inventory item by ID
+  @Put(':id')
+  update(@Param('id') id: number, @Body() data: Partial<Inventory>): Promise<Inventory> {
+    return this.inventoryService.update(id, data);
+  }
+
+  // Delete an inventory item by ID
+  @Delete(':id')
+  delete(@Param('id') id: number): Promise<void> {
+    return this.inventoryService.delete(id);
+  }
+}
