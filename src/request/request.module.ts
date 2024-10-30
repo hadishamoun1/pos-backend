1e1cc81f96a63a21809a20983847c9f12e3aa07c
hadishamoun1 <shamounhadi2@gmail.com>
2024-10-30 23:08:22 +0200

    Added module code

--- src/request/request.module.ts
+++ src/request/request.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Request } from '../entities/request.entity';
+import { RequestItem } from '../entities/request-item.entity';
 import { RequestService } from './request.service';
 import { RequestController } from './request.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Request, RequestItem])],
   providers: [RequestService],
-  controllers: [RequestController]
+  controllers: [RequestController],
 })
 export class RequestModule {}
