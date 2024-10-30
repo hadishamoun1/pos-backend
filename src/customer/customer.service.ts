567ab3e041944315b9b3ab07f95e63d0777c6144
hadishamoun1 <shamounhadi2@gmail.com>
2024-10-30 20:44:06 +0200

    Added service crud

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Customer } from '../entities/customer.entity';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(
+    @InjectRepository(Customer)
+    private readonly customerRepository: Repository<Customer>,
+  ) {}
+
+  async create(data: Partial<Customer>): Promise<Customer> {
+    const newCustomer = this.customerRepository.create(data);
+    return this.customerRepository.save(newCustomer);
+  }
+
+  async findAll(): Promise<Customer[]> {
+    return this.customerRepository.find();
+  }
+
+  async findOne(id: number): Promise<Customer> {
+    return this.customerRepository.findOne({ where: { id } });
+  }
+
+  async update(id: number, data: Partial<Customer>): Promise<Customer> {
+    await this.customerRepository.update(id, data);
+    return this.customerRepository.findOne({ where: { id } });
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.customerRepository.delete(id);
+  }
+}
