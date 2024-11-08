3c2c48ee657173736e2253a8e44fb89bef309f03
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-08 20:03:25 +0200

    Created the dimension entity file

---
+++ src/entities/inventory/dimension.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { Item } from './item.entity';
+import { AdjustedBox } from './adjustedBox.entity';
+import { OpenedSheet } from './openedSheet.entity';
+
+@Entity()
+export class Dimension {
+  @PrimaryGeneratedColumn()
+  dimensionId: number; // Unique identifier for each dimension variant
+
+  @ManyToOne(() => Item, (item) => item.dimensions)
+  item: Item; // Reference to the parent item
+
+  @Column()
+  length: number; // Length of the item in cm (e.g., 225)
+
+  @Column()
+  width: number; // Width of the item in cm (e.g., 321)
+
+  @Column()
+  sheetsPerBox: number; // Number of sheets per box for this dimension
+
+  @Column()
+  quantityUnopenedBoxes: number; // Total unopened boxes available for this dimension
+
+  @OneToMany(() => AdjustedBox, (adjustedBox) => adjustedBox.dimension)
+  adjustedBoxes: AdjustedBox[]; // Collection of boxes with adjusted sheet counts
+
+  @OneToMany(() => OpenedSheet, (openedSheet) => openedSheet.dimension)
+  openedSheets: OpenedSheet[]; // Collection of opened boxes where sheets are sold individually
+}
