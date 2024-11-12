698719bea44ded09c1c14259f1a1e8e32e88f426
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-12 20:31:15 +0200

    Created the suppliers service file

--- src/suppliers/suppliers.module.ts
+++ src/suppliers/suppliers.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Supplier } from '../entities/suppliers.entity';
 import { SupplierService } from './suppliers.service';
 import { SupplierController } from './suppliers.controller';
+import { SuppliersService } from './suppliers.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Supplier])],
-  providers: [SupplierService],
+  providers: [SupplierService, SuppliersService],
   controllers: [SupplierController],
   exports: [SupplierService],
 })
