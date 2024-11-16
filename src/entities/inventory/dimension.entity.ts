173816bbf58eee3402f4016c0205fec96bd20944
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-16 17:26:07 +0200

    Fixed naming errors

--- src/entities/inventory/dimension.entity.ts
+++ src/entities/inventory/dimension.entity.ts
@@ -9,7 +9,7 @@ import {
 import { Item } from './item.entity';
 import { AdjustedBox } from './adjustedBox.entity';
 import { OpenedSheet } from './openedSheet.entity';
-import { PurchaseItem } from '../purchaseItem.entity';
+import { PurchaseInvoiceItem } from '../purchaseItem.entity';
 
 @Entity()
 export class Dimension {
@@ -43,6 +43,6 @@ export class Dimension {
   @OneToMany(() => OpenedSheet, (openedSheet) => openedSheet.dimension)
   openedSheets: OpenedSheet[]; // Collection of opened sheets for individual sale
 
-  @OneToMany(() => PurchaseItem, (purchaseItem) => purchaseItem.dimension)
-  purchaseItems: PurchaseItem[]; // Collection of purchase items referencing this dimension
+  @OneToMany(() => PurchaseInvoiceItem, (purchaseItem) => purchaseItem.dimension)
+  purchaseItems: PurchaseInvoiceItem[]; // Collection of purchase items referencing this dimension
 }
--- src/entities/suppliers.entity.ts
+++ src/entities/suppliers.entity.ts
@@ -1,21 +1,41 @@
-// supplier.entity.ts
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Unique,
+} from 'typeorm';
 import { PurchaseInvoice } from './purchaseInvoice.entity';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 @Entity()
+@Unique(['name'])
 export class Supplier {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   name: string; // Supplier's name
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   contactInfo: string; // Contact information for the supplier
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, default: 'Not provided' })
+  @IsString()
   address: string; // Optional address for the supplier
 
-  @OneToMany(() => PurchaseInvoice, (purchaseInvoice) => purchaseInvoice.supplier)
+  @OneToMany(() => PurchaseInvoice, (purchaseInvoice) => purchaseInvoice.supplierName)
   purchaseInvoices: PurchaseInvoice[]; // Link to purchase invoices
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
