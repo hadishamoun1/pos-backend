d8d1fdd7fe6f2c901dddfe635b6490ffa9d01d39
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-12 00:33:06 +0200

    Created the categories controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { Categories } from '../entities/categories.entity';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
+  }
+
+  @Get(':categoriesCode')
+  findOne(@Param('categoriesCode') categoriesCode: string) {
+    return this.categoriesService.findOne(categoriesCode);
+  }
+
+  @Post()
+  create(@Body() categories: Categories) {
+    return this.categoriesService.create(categories);
+  }
+
+  @Put(':categoriesCode')
+  update(
+    @Param('categoriesCode') categoriesCode: string,
+    @Body() categories: Partial<Categories>,
+  ) {
+    return this.categoriesService.update(categoriesCode, categories);
+  }
+
+  @Delete(':categoriesCode')
+  delete(@Param('categoriesCode') categoriesCode: string) {
+    return this.categoriesService.delete(categoriesCode);
+  }
+}
