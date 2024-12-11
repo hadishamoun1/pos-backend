a249e2bf0daae6c1b28c6d7a7bbab177ff356c7c
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-12 00:30:19 +0200

    Created the controller classification

--- src/classification/classification.controller.ts
+++ src/classification/classification.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ClassificationService } from './classification.service';
+import { Classification } from '../entities/classification.entity';
 
-@Controller('classification')
-export class ClassificationController {}
+@Controller('classifications')
+export class ClassificationController {
+  constructor(private readonly classificationService: ClassificationService) {}
+
+  @Get()
+  findAll() {
+    return this.classificationService.findAll();
+  }
+
+  @Get(':classificationCode')
+  findOne(@Param('classificationCode') classificationCode: string) {
+    return this.classificationService.findOne(classificationCode);
+  }
+
+  @Post()
+  create(@Body() classification: Classification) {
+    return this.classificationService.create(classification);
+  }
+
+  @Put(':classificationCode')
+  update(
+    @Param('classificationCode') classificationCode: string,
+    @Body() classification: Partial<Classification>,
+  ) {
+    return this.classificationService.update(
+      classificationCode,
+      classification,
+    );
+  }
+
+  @Delete(':classificationCode')
+  delete(@Param('classificationCode') classificationCode: string) {
+    return this.classificationService.delete(classificationCode);
+  }
+}
