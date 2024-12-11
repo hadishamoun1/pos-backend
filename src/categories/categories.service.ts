8cc477e809b821342228cec1d576b268eaf30f85
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-12 00:32:15 +0200

    Created the categories service

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Categories } from '../entities/categories.entity';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Categories)
+    private readonly categoriesRepository: Repository<Categories>,
+  ) {}
+
+  findAll() {
+    return this.categoriesRepository.find();
+  }
+
+  findOne(categoriesCode: string) {
+    return this.categoriesRepository.findOneBy({ categoriesCode });
+  }
+
+  create(categories: Categories) {
+    return this.categoriesRepository.save(categories);
+  }
+
+  update(categoriesCode: string, categories: Partial<Categories>) {
+    return this.categoriesRepository.update(categoriesCode, categories);
+  }
+
+  delete(categoriesCode: string) {
+    return this.categoriesRepository.delete(categoriesCode);
+  }
+}
