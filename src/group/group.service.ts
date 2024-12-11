982cfbacca038bf5ec3e6a5914112dd9edebab0b
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-12 00:27:35 +0200

    Created the service group

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Group } from '../entities/group.entity';
 
 @Injectable()
-export class GroupService {}
+export class GroupService {
+  constructor(
+    @InjectRepository(Group)
+    private readonly groupRepository: Repository<Group>,
+  ) {}
+
+  findAll() {
+    return this.groupRepository.find();
+  }
+
+  findOne(groupCode: string) {
+    return this.groupRepository.findOneBy({ groupCode });
+  }
+
+  create(group: Group) {
+    return this.groupRepository.save(group);
+  }
+
+  update(groupCode: string, group: Partial<Group>) {
+    return this.groupRepository.update(groupCode, group);
+  }
+
+  delete(groupCode: string) {
+    return this.groupRepository.delete(groupCode);
+  }
+}
