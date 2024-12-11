6ad5de11c9e0eb3b4ef1a4cc984c54df02602b9d
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-11 23:55:16 +0200

    Created the currency entity

---
+++ src/entities/currency.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryColumn, OneToMany } from 'typeorm';
+import { CurrencyRate } from './CurrencyRate';
+
+@Entity()
+export class Currency {
+  @PrimaryColumn({ length: 3 })
+  currencyCode: string; // e.g., LL, $
+
+  @Column({ length: 50 })
+  currencyName: string; // e.g., Lebanese Pound, US Dollar
+
+  @OneToMany(() => CurrencyRate, (rate) => rate.currency)
+  exchangeRates: CurrencyRate[];
+}
