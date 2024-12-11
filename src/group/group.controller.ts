c849d6248e2ce7820ce7f581fbcf084205835d45
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-12 00:28:06 +0200

    Created the controller group

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { GroupService } from './group.service';
+import { Group } from '../entities/group.entity';
 
-@Controller('group')
-export class GroupController {}
+@Controller('groups')
+export class GroupController {
+  constructor(private readonly groupService: GroupService) {}
+
+  @Get()
+  findAll() {
+    return this.groupService.findAll();
+  }
+
+  @Get(':groupCode')
+  findOne(@Param('groupCode') groupCode: string) {
+    return this.groupService.findOne(groupCode);
+  }
+
+  @Post()
+  create(@Body() group: Group) {
+    return this.groupService.create(group);
+  }
+
+  @Put(':groupCode')
+  update(@Param('groupCode') groupCode: string, @Body() group: Partial<Group>) {
+    return this.groupService.update(groupCode, group);
+  }
+
+  @Delete(':groupCode')
+  delete(@Param('groupCode') groupCode: string) {
+    return this.groupService.delete(groupCode);
+  }
+}
