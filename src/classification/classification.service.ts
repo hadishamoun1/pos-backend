0b49a16901c45c6c6685086d75deec7e3999d285
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-12 00:29:46 +0200

    Created th service classification

--- src/classification/classification.service.ts
+++ src/classification/classification.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Classification } from '../entities/classification.entity';
 
 @Injectable()
-export class ClassificationService {}
+export class ClassificationService {
+  constructor(
+    @InjectRepository(Classification)
+    private readonly classificationRepository: Repository<Classification>,
+  ) {}
+
+  findAll() {
+    return this.classificationRepository.find();
+  }
+
+  findOne(classificationCode: string) {
+    return this.classificationRepository.findOneBy({ classificationCode });
+  }
+
+  create(classification: Classification) {
+    return this.classificationRepository.save(classification);
+  }
+
+  update(classificationCode: string, classification: Partial<Classification>) {
+    return this.classificationRepository.update(
+      classificationCode,
+      classification,
+    );
+  }
+
+  delete(classificationCode: string) {
+    return this.classificationRepository.delete(classificationCode);
+  }
+}
