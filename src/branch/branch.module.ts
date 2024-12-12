c0991d892c6355b890ff8e761970471117d436c6
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-12 15:38:03 +0200

    Created the branch service

--- src/branch/branch.module.ts
+++ src/branch/branch.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BranchService } from './branch.service';
 import { BranchController } from './branch.controller';
-import { Branch } from './branch.entity';
+import { Branch } from '../entities/branch.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Branch])],
--- src/branch/branch.service.ts
+++ src/branch/branch.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Branch } from '../entities/branch.entity';
 
 @Injectable()
-export class BranchService {}
+export class BranchService {
+  constructor(
+    @InjectRepository(Branch)
+    private readonly branchRepository: Repository<Branch>,
+  ) {}
+
+  async create(branchName: string): Promise<Branch> {
+    const branch = this.branchRepository.create({ branchName });
+    return await this.branchRepository.save(branch);
+  }
+
+  async findAll(): Promise<Branch[]> {
+    return await this.branchRepository.find();
+  }
+
+  async findOne(id: number): Promise<Branch> {
+    return await this.branchRepository.findOneBy({ id });
+  }
+
+  async update(id: number, branchName: string): Promise<Branch> {
+    const branch = await this.branchRepository.findOneBy({ id });
+    if (branch) {
+      branch.branchName = branchName;
+      return await this.branchRepository.save(branch);
+    }
+    throw new Error('Branch not found');
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.branchRepository.delete(id);
+  }
+}
