dfb3f4fb9209592d2cd9ce2f7db640407c81edd9
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-13 00:25:21 +0200

    Created the suppliers controller

--- src/suppliers/suppliers.controller.ts
+++ src/suppliers/suppliers.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { SupplierService } from './suppliers.service';
+import { Supplier } from '../entities/supplier.entity';
 
 @Controller('suppliers')
-export class SuppliersController {}
+export class SupplierController {
+  constructor(private readonly supplierService: SupplierService) {}
+
+  @Post()
+  createSupplier(@Body() supplierData: Partial<Supplier>): Promise<Supplier> {
+    return this.supplierService.createSupplier(supplierData);
+  }
+
+  @Get()
+  getAllSuppliers(): Promise<Supplier[]> {
+    return this.supplierService.getAllSuppliers();
+  }
+
+  @Get(':id')
+  getSupplierById(@Param('id') id: number): Promise<Supplier> {
+    return this.supplierService.getSupplierById(id);
+  }
+
+  @Delete(':id')
+  deleteSupplier(@Param('id') id: number): Promise<void> {
+    return this.supplierService.deleteSupplier(id);
+  }
+}
