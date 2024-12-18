8eb009cad8ef43e30c7c54d836c31b40a8771543
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-18 23:44:10 +0200

    Created the debit note controller

--- src/debit-note/debit-note.controller.ts
+++ src/debit-note/debit-note.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  NotFoundException,
+} from '@nestjs/common';
+import { DebitNoteService } from './debit-note.service';
+import { DebitNote } from '../entities/Vouchers/debitNote.entity';
 
-@Controller('debit-note')
-export class DebitNoteController {}
+@Controller('debit-notes')
+export class DebitNoteController {
+  constructor(private readonly debitNoteService: DebitNoteService) {}
+
+  @Post()
+  async createDebitNote(@Body() data: Partial<DebitNote>): Promise<DebitNote> {
+    return this.debitNoteService.createDebitNote(data);
+  }
+
+  @Get()
+  async getAllDebitNotes(): Promise<DebitNote[]> {
+    return this.debitNoteService.getAllDebitNotes();
+  }
+
+  @Get(':id')
+  async getDebitNoteById(@Param('id') id: number): Promise<DebitNote> {
+    const debitNote = await this.debitNoteService.getDebitNoteById(id);
+    if (!debitNote) {
+      throw new NotFoundException(`Debit Note with ID ${id} not found.`);
+    }
+    return debitNote;
+  }
+
+  @Put(':id')
+  async updateDebitNote(
+    @Param('id') id: number,
+    @Body() data: Partial<DebitNote>,
+  ): Promise<DebitNote> {
+    return this.debitNoteService.updateDebitNote(id, data);
+  }
+
+  @Delete(':id')
+  async deleteDebitNote(@Param('id') id: number): Promise<void> {
+    await this.debitNoteService.deleteDebitNote(id);
+  }
+}
