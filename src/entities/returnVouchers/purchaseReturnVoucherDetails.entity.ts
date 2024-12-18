7d730fbe5bba75ba833a9d92bf29c1cc9cfddfca
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-19 00:35:56 +0200

    Created the purchase return voucher entity and its details

---
+++ src/entities/returnVouchers/purchaseReturnVoucherDetails.entity.ts
@@ -0,0 +1,60 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+  } from 'typeorm';
+  import { Account } from '../account.entity';
+  import { CurrencyRate } from '../currencyRate.entity';
+  import { PurchaseReturnVoucher } from './purchaseReturnVoucher.entity';
+  
+  @Entity('purchase_return_voucher_details')
+  export class PurchaseReturnVoucherDetail {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @ManyToOne(() => PurchaseReturnVoucher, (voucher) => voucher.details, {
+      onDelete: 'CASCADE',
+    })
+    purchaseReturnVoucher: PurchaseReturnVoucher;
+  
+    @ManyToOne(() => Account, { nullable: false })
+    account: Account;
+  
+    @Column({ type: 'varchar', length: 50, nullable: true })
+    checkNumber: string;
+  
+    @Column({ type: 'date', nullable: true })
+    checkDate: Date;
+  
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    bankName: string;
+  
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    description: string;
+  
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    dr: number;
+  
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    drUSD: number;
+  
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    drLL: number;
+  
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    cr: number;
+  
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    crUSD: number;
+  
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    crLL: number;
+  
+    @ManyToOne(() => CurrencyRate, { nullable: false })
+    exchangeRateAcc: CurrencyRate;
+  
+    @ManyToOne(() => CurrencyRate, { nullable: false })
+    exchangeRateUSD: CurrencyRate;
+  }
+  
