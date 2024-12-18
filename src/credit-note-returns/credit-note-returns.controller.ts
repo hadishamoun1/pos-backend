fc0af5f0219e055de31767637c443644c1cd2d8f
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-19 01:24:17 +0200

    Created the credit note return controller

--- src/credit-note-returns/credit-note-returns.controller.ts
+++ src/credit-note-returns/credit-note-returns.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CreditNoteReturnService } from './credit-note-returns.service';
+import { CreditNoteReturn } from '../entities/returnVouchers/creditNoteReturn.entity';
 
 @Controller('credit-note-returns')
-export class CreditNoteReturnsController {}
+export class CreditNoteReturnController {
+  constructor(
+    private readonly creditNoteReturnService: CreditNoteReturnService,
+  ) {}
+
+  @Post()
+  create(@Body() data: Partial<CreditNoteReturn>): Promise<CreditNoteReturn> {
+    return this.creditNoteReturnService.createCreditNoteReturn(data);
+  }
+
+  @Get()
+  getAll(): Promise<CreditNoteReturn[]> {
+    return this.creditNoteReturnService.getAllCreditNoteReturns();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: number): Promise<CreditNoteReturn> {
+    return this.creditNoteReturnService.getCreditNoteReturnById(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() data: Partial<CreditNoteReturn>,
+  ): Promise<CreditNoteReturn> {
+    return this.creditNoteReturnService.updateCreditNoteReturn(id, data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Promise<void> {
+    return this.creditNoteReturnService.deleteCreditNoteReturn(id);
+  }
+}
