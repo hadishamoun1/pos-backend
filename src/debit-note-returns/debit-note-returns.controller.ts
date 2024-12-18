8928aa5891beafec3bb002ddb501b5fa7463a0cb
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-19 01:13:13 +0200

    Created the controller for debit note return

--- src/debit-note-returns/debit-note-returns.controller.ts
+++ src/debit-note-returns/debit-note-returns.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('debit-note-returns')
-export class DebitNoteReturnsController {}
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Param,
+    Body,
+  } from '@nestjs/common';
+  import { DebitNoteReturnService } from './debit-note-returns.service';
+  import { DebitNoteReturn } from '../entities/returnVouchers/debitNoteReturn.entity';
+  
+  @Controller('debit-note-returns')
+  export class DebitNoteReturnController {
+    constructor(
+      private readonly debitNoteReturnService: DebitNoteReturnService,
+    ) {}
+  
+    @Post()
+    create(@Body() data: Partial<DebitNoteReturn>): Promise<DebitNoteReturn> {
+      return this.debitNoteReturnService.createDebitNoteReturn(data);
+    }
+  
+    @Get()
+    getAll(): Promise<DebitNoteReturn[]> {
+      return this.debitNoteReturnService.getAllDebitNoteReturns();
+    }
+  
+    @Get(':id')
+    getById(@Param('id') id: number): Promise<DebitNoteReturn> {
+      return this.debitNoteReturnService.getDebitNoteReturnById(id);
+    }
+  
+    @Put(':id')
+    update(
+      @Param('id') id: number,
+      @Body() data: Partial<DebitNoteReturn>,
+    ): Promise<DebitNoteReturn> {
+      return this.debitNoteReturnService.updateDebitNoteReturn(id, data);
+    }
+  
+    @Delete(':id')
+    delete(@Param('id') id: number): Promise<void> {
+      return this.debitNoteReturnService.deleteDebitNoteReturn(id);
+    }
+  }
+  
