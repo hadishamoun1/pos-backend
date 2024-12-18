a7076ae3212109907f419832274c3eb9e8d9b5cc
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-18 14:06:02 +0200

    Created entities dor debit note and its details

---
+++ src/entities/Vouchers/debitNote.entity.ts
@@ -0,0 +1,54 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { Account } from '../account.entity';
+import { CurrencyRate } from '../currencyRate.entity';
+import { DebitNoteDetail } from './debitNoteDetails.entity';
+
+@Entity('journal_vouchers')
+export class DebitNote {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @ManyToOne(() => Account, { nullable: false })
+  account: Account;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  jvNumber: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDr: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDrUSD: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDrLL: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCr: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCrUSD: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCrLL: number;
+
+  @ManyToOne(() => CurrencyRate, { nullable: false })
+  exchangeRateAcc: CurrencyRate;
+
+  @ManyToOne(() => CurrencyRate, { nullable: false })
+  exchangeRateUSD: CurrencyRate;
+
+  @OneToMany(() => DebitNoteDetail, (detail) => detail.debitNote, {
+    cascade: true,
+  })
+  details: DebitNoteDetail[];
+}
