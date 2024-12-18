75effafeb67ace403bcd392e34f893ae113c68bc
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-19 01:08:08 +0200

    Created the debit note return entity and details

---
+++ src/entities/returnVouchers/debitNoteReturn.entity.ts
@@ -0,0 +1,54 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { Account } from '../account.entity';
+import { CurrencyRate } from '../currencyRate.entity';
+import { DebitNoteReturnDetail } from './debitNoteReturnDetails.entity';
+
+@Entity('debit_note_returns')
+export class DebitNoteReturn {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @ManyToOne(() => Account, { nullable: false })
+  account: Account;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  dnReturnNumber: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDr: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDrUSD: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDrLL: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCr: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCrUSD: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCrLL: number;
+
+  @ManyToOne(() => CurrencyRate, { nullable: false })
+  exchangeRateAcc: CurrencyRate;
+
+  @ManyToOne(() => CurrencyRate, { nullable: false })
+  exchangeRateUSD: CurrencyRate;
+
+  @OneToMany(() => DebitNoteReturnDetail, (detail) => detail.debitNoteReturn, {
+    cascade: true,
+  })
+  details: DebitNoteReturnDetail[];
+}
---
+++ src/entities/returnVouchers/debitNoteReturnDetails.entity.ts
@@ -0,0 +1,59 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+} from 'typeorm';
+import { Account } from '../account.entity';
+import { CurrencyRate } from '../currencyRate.entity';
+import { DebitNoteReturn } from './debitNoteReturn.entity';
+
+@Entity('debit_note_return_details')
+export class DebitNoteReturnDetail {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => DebitNoteReturn, (noteReturn) => noteReturn.details, {
+    onDelete: 'CASCADE',
+  })
+  debitNoteReturn: DebitNoteReturn;
+
+  @ManyToOne(() => Account, { nullable: false })
+  account: Account;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  checkNumber: string;
+
+  @Column({ type: 'date', nullable: true })
+  checkDate: Date;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  bankName: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  description: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  dr: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  drUSD: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  drLL: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  cr: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  crUSD: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  crLL: number;
+
+  @ManyToOne(() => CurrencyRate, { nullable: false })
+  exchangeRateAcc: CurrencyRate;
+
+  @ManyToOne(() => CurrencyRate, { nullable: false })
+  exchangeRateUSD: CurrencyRate;
+}
