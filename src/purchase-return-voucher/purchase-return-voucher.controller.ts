3b04d728b53ae09b948a6a65e390249c6aef053c
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-19 00:41:55 +0200

    Created the purchase return voucher controller

--- src/purchase-return-voucher/purchase-return-voucher.controller.ts
+++ src/purchase-return-voucher/purchase-return-voucher.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('purchase-return-voucher')
-export class PurchaseReturnVoucherController {}
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Param,
+    Body,
+  } from '@nestjs/common';
+  import { PurchaseReturnVoucherService } from './purchase-return-voucher.service';
+  import { PurchaseReturnVoucher } from '../entities/returnVouchers/purchaseReturnVoucher.entity';
+  
+  @Controller('purchase-return-vouchers')
+  export class PurchaseReturnVoucherController {
+    constructor(
+      private readonly purchaseReturnVoucherService: PurchaseReturnVoucherService,
+    ) {}
+  
+    @Post()
+    create(
+      @Body() data: Partial<PurchaseReturnVoucher>,
+    ): Promise<PurchaseReturnVoucher> {
+      return this.purchaseReturnVoucherService.createPurchaseReturnVoucher(data);
+    }
+  
+    @Get()
+    getAll(): Promise<PurchaseReturnVoucher[]> {
+      return this.purchaseReturnVoucherService.getAllPurchaseReturnVouchers();
+    }
+  
+    @Get(':id')
+    getById(@Param('id') id: number): Promise<PurchaseReturnVoucher> {
+      return this.purchaseReturnVoucherService.getPurchaseReturnVoucherById(id);
+    }
+  
+    @Put(':id')
+    update(
+      @Param('id') id: number,
+      @Body() data: Partial<PurchaseReturnVoucher>,
+    ): Promise<PurchaseReturnVoucher> {
+      return this.purchaseReturnVoucherService.updatePurchaseReturnVoucher(id, data);
+    }
+  
+    @Delete(':id')
+    delete(@Param('id') id: number): Promise<void> {
+      return this.purchaseReturnVoucherService.deletePurchaseReturnVoucher(id);
+    }
+  }
+  
