955b2a87edd73693e999ab8fb9062cdc95552295
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-19 00:06:56 +0200

    Created receipt voucher return controller

--- src/receipt-voucher-return/receipt-voucher-return.controller.ts
+++ src/receipt-voucher-return/receipt-voucher-return.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ReceiptVoucherReturnService } from './receipt-voucher-return.service';
+import { ReceiptVoucherReturn } from '../entities/returnVouchers/receiptVoucherReturn.entity';
 
-@Controller('receipt-voucher-return')
-export class ReceiptVoucherReturnController {}
+@Controller('receipt-voucher-returns')
+export class ReceiptVoucherReturnController {
+  constructor(
+    private readonly receiptVoucherReturnService: ReceiptVoucherReturnService,
+  ) {}
+
+  @Post()
+  create(
+    @Body() data: Partial<ReceiptVoucherReturn>,
+  ): Promise<ReceiptVoucherReturn> {
+    return this.receiptVoucherReturnService.createReceiptVoucherReturn(data);
+  }
+
+  @Get()
+  getAll(): Promise<ReceiptVoucherReturn[]> {
+    return this.receiptVoucherReturnService.getAllReceiptVoucherReturns();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: number): Promise<ReceiptVoucherReturn> {
+    return this.receiptVoucherReturnService.getReceiptVoucherReturnById(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() data: Partial<ReceiptVoucherReturn>,
+  ): Promise<ReceiptVoucherReturn> {
+    return this.receiptVoucherReturnService.updateReceiptVoucherReturn(
+      id,
+      data,
+    );
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Promise<void> {
+    return this.receiptVoucherReturnService.deleteReceiptVoucherReturn(id);
+  }
+}
