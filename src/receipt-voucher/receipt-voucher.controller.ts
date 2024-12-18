26a8a25cc6686e8fd48ced941e90007a27a85b1a
hadishamoun1 <shamounhadi2@gmail.com>
2024-12-18 11:27:03 +0200

    Created the controller file for receipt voucher

--- src/receipt-voucher/receipt-voucher.controller.ts
+++ src/receipt-voucher/receipt-voucher.controller.ts
@@ -1,4 +1,72 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('receipt-voucher')
-export class ReceiptVoucherController {}
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Param,
+    Body,
+    HttpException,
+    HttpStatus,
+  } from '@nestjs/common';
+  import { ReceiptVoucherService } from './receipt-voucher.service';
+  import { ReceiptVoucher } from '../entities/Vouchers/recieptVoucher.entity';
+  
+  @Controller('receipt-vouchers')
+  export class ReceiptVoucherController {
+    constructor(private readonly receiptVoucherService: ReceiptVoucherService) {}
+  
+    // Create a new Receipt Voucher
+    @Post()
+    async createReceiptVoucher(@Body() data: Partial<ReceiptVoucher>): Promise<ReceiptVoucher> {
+      try {
+        return await this.receiptVoucherService.createReceiptVoucher(data);
+      } catch (error) {
+        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+      }
+    }
+  
+    // Get all Receipt Vouchers
+    @Get()
+    async getAllReceiptVouchers(): Promise<ReceiptVoucher[]> {
+      try {
+        return await this.receiptVoucherService.getAllReceiptVouchers();
+      } catch (error) {
+        throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
+  
+    // Get a single Receipt Voucher by ID
+    @Get(':id')
+    async getReceiptVoucherById(@Param('id') id: number): Promise<ReceiptVoucher> {
+      try {
+        return await this.receiptVoucherService.getReceiptVoucherById(id);
+      } catch (error) {
+        throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+      }
+    }
+  
+    // Update a Receipt Voucher
+    @Put(':id')
+    async updateReceiptVoucher(
+      @Param('id') id: number,
+      @Body() data: Partial<ReceiptVoucher>,
+    ): Promise<ReceiptVoucher> {
+      try {
+        return await this.receiptVoucherService.updateReceiptVoucher(id, data);
+      } catch (error) {
+        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+      }
+    }
+  
+    // Delete a Receipt Voucher
+    @Delete(':id')
+    async deleteReceiptVoucher(@Param('id') id: number): Promise<void> {
+      try {
+        await this.receiptVoucherService.deleteReceiptVoucher(id);
+      } catch (error) {
+        throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+      }
+    }
+  }
+  
