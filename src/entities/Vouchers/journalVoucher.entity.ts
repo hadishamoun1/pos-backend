8ddc2ae50a72bee8b41ffdf86206d10c186f08db
hadishamoun1 <shamounhadi2@gmail.com>
2025-01-19 19:39:27 +0200

    Added type to indicate if the jv is S or G

--- src/entities/Vouchers/journalVoucher.entity.ts
+++ src/entities/Vouchers/journalVoucher.entity.ts
@@ -1,55 +1,57 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-    OneToMany,
-  } from 'typeorm';
-  import { Account } from '../account.entity';
-  import { CurrencyRate } from '../currencyRate.entity';
-  import { JournalVoucherDetail } from './journalVoucherDetails.entity';
-  
-  @Entity('journal_vouchers')
-  export class JournalVoucher {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column({ type: 'date' })
-    date: Date;
-  
-    @ManyToOne(() => Account, { nullable: false })
-    account: Account;
-  
-    @Column({ type: 'varchar', length: 50, unique: true })
-    jvNumber: string;
-  
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    totalDr: number;
-  
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    totalDrUSD: number;
-  
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    totalDrLL: number;
-  
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    totalCr: number;
-  
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    totalCrUSD: number;
-  
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    totalCrLL: number;
-  
-    @ManyToOne(() => CurrencyRate, { nullable: true })
-    exchangeRateAcc: CurrencyRate;
-  
-    @ManyToOne(() => CurrencyRate, { nullable: true })
-    exchangeRateUSD: CurrencyRate;
-  
-    @OneToMany(() => JournalVoucherDetail, (detail) => detail.journalVoucher, {
-      cascade: true,
-    })
-    details: JournalVoucherDetail[];
-  }
-  
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { Account } from '../account.entity';
+import { CurrencyRate } from '../currencyRate.entity';
+import { JournalVoucherDetail } from './journalVoucherDetails.entity';
+
+@Entity('journal_vouchers')
+export class JournalVoucher {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @ManyToOne(() => Account, { nullable: false })
+  account: Account;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  jvNumber: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDr: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDrUSD: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalDrLL: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCr: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCrUSD: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalCrLL: number;
+
+  @Column({ type: 'varchar', length: 1})
+  jvType: string;
+
+  @ManyToOne(() => CurrencyRate, { nullable: true })
+  exchangeRateAcc: CurrencyRate;
+
+  @ManyToOne(() => CurrencyRate, { nullable: true })
+  exchangeRateUSD: CurrencyRate;
+
+  @OneToMany(() => JournalVoucherDetail, (detail) => detail.journalVoucher, {
+    cascade: true,
+  })
+  details: JournalVoucherDetail[];
+}
