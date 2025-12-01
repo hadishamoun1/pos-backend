b060c5cfa0b34b7c1500c32289c331a2cb8a3482
Hadi Shamoun <shamounhadi1@gmail.com>
2025-12-01 12:18:08 +0200

    Fixed entity naming

--- src/entities/invoice.entity.ts
+++ src/entities/invoice.entity.ts
@@ -10,7 +10,7 @@ import { Customer } from './customer.entity';
 import { Branch } from './branch.entity';
 import { Currency } from './currency.entity';
 import { InvoiceItem } from './invoiceItem.entity';
-import { SqmPiece } from '../entities/inventory/sqmPiece.entity';
+import { SqmPiece } from './inventory/sqmPiece.entity';
 
 @Entity('invoices')
 export class Invoice {
