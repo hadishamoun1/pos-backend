ae971a8d7802d277b0122e4063980d009a8fc141
Hadi Shamoun <shamounhadi1@gmail.com>
2026-04-28 14:02:12 +0300

    Fixed users face login

--- src/main.ts
+++ src/main.ts
@@ -34,10 +34,10 @@ async function bootstrap() {
 
       if (/^https:\/\/.*\.vercel\.app$/.test(origin)) return callback(null, true);
 
-      if (/^http:\/\/192\.168\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
-      if (/^http:\/\/10\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
-      if (/^http:\/\/172\.(1[6-9]|2\d|3[0-1])\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
-      if (/^http:\/\/26\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
+      if (/^https?:\/\/192\.168\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
+      if (/^https?:\/\/10\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
+      if (/^https?:\/\/172\.(1[6-9]|2\d|3[0-1])\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
+      if (/^https?:\/\/26\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
 
       return callback(new Error(`CORS blocked origin: ${origin}`), false);
     },
