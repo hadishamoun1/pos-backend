b7eee9d88cbef5f44ac3775e2eecbe91abfbfef8
Hadi Shamoun <shamounhadi1@gmail.com>
2026-05-29 13:58:34 +0300

    Added cors

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,8 @@ async function bootstrap() {
       if (/^https?:\/\/.*\.revopos\.net$/.test(origin)) return callback(null, true);
       if (origin === 'https://revopos.net') return callback(null, true);
       if (origin === 'https://shamounco.org') return callback(null, true);
+      if (origin === 'https://alishamoun.com') return callback(null, true);
+      if (origin === 'https://www.alishamoun.com') return callback(null, true);
 
       if (/^https?:\/\/192\.168\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
       if (/^https?:\/\/10\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(origin)) return callback(null, true);
